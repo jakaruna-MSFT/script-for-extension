5a3c566f220f1d5f487de609a8ad5185639ab6b9
jakaruna-MSFT <43405697+jakaruna-MSFT@users.noreply.github.com>
2018-12-03 12:31:44 +0530

    Update errorTest.ps1

--- errorTest.ps1
+++ errorTest.ps1
@@ -2,5 +2,10 @@ Write-Host "adding current date to a file"
 
 Get-Date | Add-Content C:\extension_execution_test.txt
 
-#below command is ues to generate the error during the execution of the extension.
-SomeWrongCommandToGenerateError
+try {
+    #below command is ues to generate the error during the execution of the extension.
+    SomeWrongCommandToGenerateError
+}
+catch {
+    exit   500
+}
